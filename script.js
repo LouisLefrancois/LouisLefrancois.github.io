88dfaf97bd80d2dc0bf3b15f5fdfc19ba7190f47
Louislf <lefrancois_louis@yahoo.fr>
2021-12-06 04:24:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -148,3 +148,32 @@ document.onclick = function(e) {
         blurred.classList.remove('activeBlurred')
     }
 }
+
+
+
+
+
+
+
+
+
+//Get all the hyperlink elements
+var links = document.getElementsByTagName("a");
+
+//Browse the previously created array
+Array.prototype.forEach.call(links, function(elem, index) {
+    //Get the hyperlink target and if it refers to an id go inside condition
+    var elemAttr = elem.getAttribute("href");
+    if (elemAttr && elemAttr.includes("#")) {
+        //Replace the regular action with a scrolling to target on click
+        elem.addEventListener("click", function(ev) {
+            ev.preventDefault();
+            //Scroll to the target element using replace() and regex to find the href's target id
+            document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+                behavior: "smooth",
+                block: "start",
+                inline: "nearest"
+            });
+        });
+    }
+});
